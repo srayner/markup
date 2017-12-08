4d2240c28503ce4220e0c88443089b91b7a03210
Steve Rayner <srayner02@gmail.com>
2017-12-08 00:00:11 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+MARK UP
+=======
+
+Takes a json string and adds html markup so that syntax highlighting
+can be applied with a simple stylesheet.
+
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+function objToHtml($obj, $indent)
+{
+    $html =  str_repeat(' ', $indent) . '{<br>';
+    foreach(get_object_vars($obj) as $key => $value) {
+        $html .= keyToHtml($key, $indent + 4);
+        $html .= valueToHtml($value, $indent, true);
+        $html .= ',<br>';
+    }
+    $html = substr($html, 0, -5);
+    $html .= '<br>' .  str_repeat(' ', $indent) . '}';
+    return $html;
+}
+
+function keyToHtml($key, $indent)
+{
+    return str_repeat(' ', $indent) . '<span class="json-property">"' . $key . '"</span>: ';
+}
+
+function valueToHtml($value, $indent, $supress)
+{
+    if (is_object($value)) {
+        return objToHtml($value, $indent + 4);
+    }
+    if (is_string($value)) {
+        $html = $supress ? '' : str_repeat(' ', $indent);
+        $html .= '<span class="json-string">"' . $value .'"</span>';
+        return $html;
+    }
+    if (is_numeric($value)) {
+        $html = $supress ? '' : str_repeat(' ', $indent);
+        $html .= '<span class="json-numeric">' . $value .'</span>';
+        return $html;
+    }
+    if (is_array($value)) {
+        $html = '[<br>';
+        foreach ($value as $subValue) {
+            $html .= valueToHtml($subValue, $indent + 4, false);
+            $html .= ',<br>';
+        }
+        $html = substr($html, 0, -5);
+        $html .= '<br>' . str_repeat(' ', $indent) . ']';
+        return $html;
+    }
+    return ''; // should never happen
+}
+    
+$json = file_get_contents('example.json');
+$obj = json_decode($json);
+$html = '<pre>';
+$html .= objToHtml($obj, 0);
+$html .= '</pre>';
+
+include 'template.phtml';
+
+
